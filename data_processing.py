9accdbf5850a14eea50f628587871fafc088eb76
phoenix1298 <akshat.patel03@gmail.com>
2024-12-10 22:33:40 +0530

    corrected the code for sum func and added a func for muti

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,13 @@
 print("add all the data processing code related fraud detection")
+
+a=5
+b=6
+def sum(a,b):
+    c=a+b
+    return c
+print("sum func")
+
+def multi(a,b):
+    d=a*b
+    return d
+print("multi funct")
